af04db8a1469d56b699dfa01c4a695fa9cd62071
josiascabreraaltamirano <127502262+josiascabreraaltamirano@users.noreply.github.com>
2024-04-26 17:27:16 +0800

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,17 +1,23 @@
+//my libraries
 #include "stat_reader.h"
-
+//std libraries
 #include <iostream>
 #include <iomanip>
 
 namespace catalogue {    
     namespace output {
+
         using namespace ::std::literals;
+
         namespace detail {
             //Parse the request and return the type (Bus or Stop)
             RequestInfo ParseRequest(std::string_view string) {
+                //simplify npos use
+                const auto np = std::string_view::npos;
+                //no space first pos
                 auto start = string.find_first_not_of(' ');
                 std::string_view type;
-                if (start == string.npos || 
+                if (start == np || 
                 (string.substr(start, (type = "Bus"sv).size() + 1) == "Bus "sv ? 0 : 
                 string.substr(start, (type = "Stop"sv).size() + 1) == "Stop "sv ? 0 : 1)) {
                     return RequestInfo{};
@@ -21,9 +27,9 @@ namespace catalogue {
                 const auto raw_route_name = string.substr(start + type.size());
                 //asigna la posicion, tomando como referencia la linea del nombre de la ruta
                 auto new_start = raw_route_name.find_first_not_of(' '); 
-                
-                return new_start != raw_route_name.npos ? 
-                RequestInfo{type, raw_route_name.substr(new_start, raw_route_name.find_last_not_of(' ') + 1 - new_start)} : 
+                auto new_end = new_start == np ? np : raw_route_name.find_last_not_of(' ');
+                return new_start != np && new_end != np ? 
+                RequestInfo{type, raw_route_name.substr(new_start, new_end + 1 - new_start)} : 
                 RequestInfo{type, std::string_view{}};
             }
         } //namespace detail
@@ -49,7 +55,8 @@ namespace catalogue {
                 auto result = transport_catalogue.GetRouteInfo(request_info.name);
                 result.total_stops == 0 ? (output << "not found"sv) :
                 (output << result.total_stops << " stops on route, "sv << result.unique_stops <<
-                std::setprecision(6) << " unique stops, "sv << result.route_length << " route length"sv);
+                std::setprecision(6) << " unique stops, "sv << result.length << " route length, "sv 
+                << result.curvature << " curvature"sv);
                 output << '\n';
                 //finish    
                 return;
