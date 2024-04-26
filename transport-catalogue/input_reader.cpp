080287a5029981fa8299997a7612158bbeef9fb0
josiascabreraaltamirano <127502262+josiascabreraaltamirano@users.noreply.github.com>
2024-04-26 17:26:17 +0800

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,5 +1,6 @@
+//my libraries
 #include "input_reader.h"
-
+//std libraries
 #include <algorithm>
 #include <cassert>
 #include <iterator>
@@ -10,20 +11,23 @@ namespace catalogue {
             /**
             * Парсит строку вида "10.123,  -30.1837" и возвращает пару координат (широта, долгота)
             */
-            geo::Coordinates ParseCoordinates(std::string_view str) {
+            geo::Coordinates ParseCoordinates(std::string_view string) {
                 static const double nan = std::nan("");
 
-                auto not_space = str.find_first_not_of(' ');
-                auto comma = str.find(',');
+                auto not_space = string.find_first_not_of(' ');
+                auto comma = string.find(',');
 
-                if (comma == str.npos) {
+                if (comma == string.npos) {
                     return {nan, nan};
                 }
 
-                auto not_space2 = str.find_first_not_of(' ', comma + 1);
+                auto not_space2 = string.find_first_not_of(' ', comma + 1);
+                auto comma2 = string.find_first_of(',', not_space2 + 1);
 
-                double lat = std::stod(std::string(str.substr(not_space, comma - not_space)));
-                double lng = std::stod(std::string(str.substr(not_space2)));
+                double lat = std::stod(std::string(string.substr(not_space, comma - not_space)));
+                double lng = std::stod(std::string(comma2 == std::string_view::npos ? 
+                                                   string.substr(not_space2) : 
+                                                   string.substr(not_space2, comma2 - not_space2)));
 
                 return {lat, lng};
             }
@@ -96,13 +100,70 @@ namespace catalogue {
                         std::string(line.substr(not_space, colon_pos - not_space)),
                         std::string(line.substr(colon_pos + 1))};
             }
+
+            //added in sprint 9
+            //Helper function to parse distance data
+            std::vector<DistanceData> ParseDistanceData(std::string_view string , std::string_view from) {
+                using namespace std::literals;
+
+                //value to return
+                std::vector<DistanceData> distances;
+                //raw distance lines 
+                const auto lines = Split(string, ',');
+                //reserve memory
+                distances.reserve(lines.size());
+                
+                //simplify npos use
+                const auto np = std::string_view::npos;
+                //key_word to find distance components
+                std::string_view key_word = " to "sv;
+                //parse each line
+                for (auto line : lines) {
+                    auto kw_pos = line.find(key_word);
+                    auto no_space = kw_pos == np ? np : line.find_first_not_of(' ' , kw_pos + key_word.size());
+                    if (kw_pos == np || no_space == np) {
+                        continue;
+                    }
+                    distances.push_back({std::string(from), std::string(line.substr(no_space)), std::stoi(std::string{line.substr(0, kw_pos - 1)})});
+                }
+                return distances;
+            }
+
+            //added in sprint 9
+            std::string_view ParseDistanceLine(std::string_view string) {
+                //simplify npos use
+                const auto np = std::string_view::npos;
+                //position of first comma for coordinates
+                auto comma1 = string.find_first_of(',');
+                //position of comma after coordinates
+                auto comma2 = comma1 == np ? np : string.find_first_of(',', comma1 + 1);
+                if (comma1 == np || comma2 == np) {
+                    return std::string_view{};
+                }
+                //in this pos starts the command
+                auto begin = string.find_first_not_of(' ', comma2 + 1);
+                return begin == np ? std::string_view{} : string.substr(begin);
+            }
         } //namespace detail
+
         void InputReader::ParseLine(std::string_view line) {
             auto command_description = detail::ParseCommandDescription(line);
             if (command_description) {
                 //if Stop
                 if (command_description.command.size() == 4) {
                     stop_commands_.push_back(std::move(command_description));
+                    //added in sprint 9
+                    const auto& last_elem = stop_commands_.back();
+                    //parse the description for getting a distance line 
+                    auto distance_line = detail::ParseDistanceLine(last_elem.description);
+                    //check if distance line contains any distance data
+                    if (!distance_line.empty()) {
+                        //create the object and move it to the queue
+                        auto distance_data = detail::ParseDistanceData(distance_line , last_elem.id);
+                        for (auto i = distance_data.rbegin(); i != distance_data.rend(); i++) {
+                            distance_commands_.push_back(std::move(*i));
+                        }
+                    }
                     return;
                 }
                 //otherwise Bus
@@ -112,12 +173,15 @@ namespace catalogue {
 
         void InputReader::ApplyCommands([[maybe_unused]] database::TransportCatalogue& catalogue) const {
             for (const auto& c : stop_commands_) {
-                    catalogue.AddStop(c.id, detail::ParseCoordinates(c.description));
+                catalogue.AddStop(c.id, detail::ParseCoordinates(c.description));
             }
 
             for (const auto& c : bus_commands_) {
-                    auto route = detail::ParseRoute(c.description);
-                    catalogue.AddRoute(c.id, route);
+                catalogue.AddRoute(c.id, detail::ParseRoute(c.description));
+            }
+
+            for (const auto& c : distance_commands_) {
+                catalogue.AddDistance(c.from, c.to, c.distance);
             }     
         } 
     } //namespace input
